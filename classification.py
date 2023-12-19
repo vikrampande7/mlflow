9868a7c4fa60a617fc65d2951c6f8474bdfc866f
Vikram Pande <vikrampande783@gmail.com>
2023-12-20 00:02:32 +0530

    logging artifacts

--- classification.py
+++ classification.py
@@ -8,6 +8,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
 from sklearn.preprocessing import LabelEncoder
 import mlflow.sklearn
+import os
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -40,13 +41,18 @@ if __name__ == "__main__":
         logger.exception(
             "IRIS dataset not found, check import statement %s", e
         )
-
+    # os.mkdir('data/')
+    np.save('data/iris.arr', iris_data)
     X = iris_data.data
     y = iris_data.target
 
     train_X, test_X, train_Y, test_Y = train_test_split(
         X, y, test_size=0.3
     )
+    np.save('data/train_X.arr', train_X)
+    np.save('data/train_Y.arr', train_Y)
+    np.save('data/test_X.arr', test_X)
+    np.save('data/test_Y.arr', test_Y)
 
     enc = LabelEncoder()
     train_Y = enc.fit_transform(train_Y)
@@ -56,7 +62,7 @@ if __name__ == "__main__":
     min_samples_split = args.min_samples_split
     min_samples_leaf = args.min_samples_leaf
 
-    exp = mlflow.set_experiment(experiment_name="Experiment_3")
+    exp = mlflow.set_experiment(experiment_name="Experiment_4")
 
     with mlflow.start_run(experiment_id=exp.experiment_id):
         model = DecisionTreeClassifier(criterion=criterion,
@@ -87,8 +93,11 @@ if __name__ == "__main__":
         }
         mlflow.log_params(params)
         mlflow.log_metrics(metrics)
-
+        mlflow.log_artifacts("data/")
         mlflow.sklearn.log_model(model, "ClassificationModel")
+
+        artifactUri = mlflow.get_artifact_uri()
+        print(f"The Artifact URI is: {artifactUri}")
 
     run = mlflow.last_active_run()
     print(f"Last Active Run Name: {run.info.run_name}")
